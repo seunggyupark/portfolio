647efe6b40e427d95df078258928bda0b29f721f
Seung Park <park@seung.dev>
2020-10-16 09:49:12 -0700

    Added Matt's testimonial

---
+++ src/components/Images/Matt.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import matt from '../../assets/img/Matt.PNG';
+
+
+const image = () => (
+    <div>
+        <img src={matt} alt="Matt" style={{width: '200px', borderRadius: '50%'}}/>
+    </div>
+);
+
+export default image;
